4ed3c92c71721a712d6be1d02fab18976c969da6
ubunturifa <jstrfaheem065@gmail.com>
2019-06-29 16:54:11 +0530

    https://www.hackerrank.com/challenges/for-loop-in-c/problem,

--- main.c
+++ main.c
@@ -1,6 +1,80 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+
+int main()
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+
+    switch (a){
+        case 1:
+            printf("one\n");
+            if (b==1){
+                break;
+            }
+
+        case 2:
+            printf("two\n");
+            if (b==2){
+                break;
+            }
+
+        case 3:
+            printf("three\n");
+            if (b==3){
+                break;
+            }
+
+        case 4:
+            printf("four\n");
+            if (b==4){
+                break;
+            }
+
+        case 5:
+            printf("five\n");
+            if (b==5){
+                break;
+            }
+
+        case 6:
+            printf("six\n");
+            if (b==6){
+                break;
+            }
+
+        case 7:
+            printf("seven\n");
+            if (b==7){
+                break;
+            }
+
+        case 8:
+            printf("eight\n");
+            if (b==8){
+                break;
+            }
+
+        case 9:
+            printf("nine\n");
+            if (b==9){
+                break;
+            }
+
+        default:
+            for (int i = 10; i <= b; i++) {
+                if(i%2!=0){
+                    printf("odd\n");
+                } else{
+                    printf("even\n");
+                }
+            }
+    }
+
     return 0;
 }
+
