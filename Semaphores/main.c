699425603f925d86bb77610e4e9b037415fb19fd
ubunturifa <jstrfaheem065@gmail.com>
2019-07-28 12:21:02 +0530

    Semaphores,

--- Semaphores/main.c
+++ Semaphores/main.c
@@ -29,17 +29,6 @@ int main()
 
 */
 
-/***********************************************************
-* You can use all the programs on  www.c-program-example.com
-* for personal and learning purposes. For permissions to use the
-* programs for commercial purposes,
-* contact info@c-program-example.com
-* To find more C programs, do visit www.c-program-example.com
-* and browse!
-*
-*                      Happy Coding
-***********************************************************/
-
 #include<stdio.h>
 #include<semaphore.h>
 #include<pthread.h>
