c5730c62d23144db55c75e7dd031ed85d1486b2d
FreeDoM <jstrfaheem065@gmail.com>
2022-09-04 16:23:41 +0530

    Two sorting techniques added,

--- Q1/main.c
+++ Q1/main.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 // Represent each row of the text.txt file
-typedef struct row {
+typedef struct row
+{
     // string
     char column0[10];
     // column two and three
@@ -13,9 +14,92 @@ typedef struct row {
     double column3;
 } Row;
 
-int main() {
+void swapRow(Row *a, Row *b)
+{
+    Row temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Selection sort ascending order
+void selectionSortRowsAsc(Row *rows, int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < length; j++)
+        {
+            if (strcmp(rows[j].column0, rows[minIndex].column0) < 0)
+                minIndex = j;
+        }
+        swapRow(&rows[minIndex], &rows[i]);
+    }
+}
+
+// Selection sort descending order
+void selectionSortRowsDesc(Row *rows, int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        int maxIndex = i;
+        for (int j = i + 1; j < length; j++)
+        {
+            if (strcmp(rows[j].column0, rows[maxIndex].column0) > 0)
+                maxIndex = j;
+        }
+        swapRow(&rows[maxIndex], &rows[i]);
+    }
+}
+
+// Bubble sort ascending order
+void bubbleSortRowsAsc(Row *rows, int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = i + 1; j < length; j++)
+        {
+            if (strcmp(rows[i].column0, rows[j].column0) < 0)
+            {
+                Row temp = rows[i];
+                rows[i] = rows[j];
+                rows[j] = temp;
+            }
+        }
+    }
+}
+
+// Bubble sort descending order
+void bubbleSortRowsDesc(Row *rows, int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = i + 1; j < length; j++)
+        {
+            if (strcmp(rows[i].column0, rows[j].column0) < 0)
+            {
+                Row temp = rows[i];
+                rows[i] = rows[j];
+                rows[j] = temp;
+            }
+        }
+    }
+}
+
+void displayRows(Row *rows, int length)
+{
+    printf("********** ROWS ITEM **********\n");
+    for (int i = 0; i < length; i++)
+    {
+        printf("%s %d %d %.2f\n", rows[i].column0, rows[i].column1, rows[i].column2, rows[i].column3);
+    }
+    printf("********** ROWS ITEM **********\n");
+}
+
+void main()
+{
     // Array of struct to represent the whole text.txt file
-    Row rows[10];
+    int rowsLength = 10;
+    Row rows[rowsLength];
     // Opening the file pointer
     FILE *fp;
     // Line buffer size
@@ -28,10 +112,10 @@ int main() {
     // Track current line
     int i = 0;
     // Read the whole file
-    while (getline(&line, &len, fp) != -1) {
-        printf("%s\n", line);
+    while (getline(&line, &len, fp) != -1)
+    {
         char *token;
-        // Tokenizing the line and assigning it into particulate row
+        // Tokenizing the line and assigning it into particulate column variable
         token = strtok(line, " ");
         strcpy(rows[i].column0, token);
         rows[i].column1 = atol(strtok(NULL, " "));
@@ -41,5 +125,16 @@ int main() {
     }
     // Closing the file pointer
     fclose(fp);
-    return 0;
+
+    // Displaying the content
+    //    displayRows(rows, rowsLength);
+    //    bubbleSortRowsAsc(rows, rowsLength);
+    //    displayRows(rows, rowsLength);
+    //        bubbleSortRowsDesc(rows, rowsLength);
+    //    displayRows(rows, rowsLength);
+
+    selectionSortRowsAsc(rows, rowsLength);
+    displayRows(rows, rowsLength);
+    selectionSortRowsDesc(rows, rowsLength);
+    displayRows(rows, rowsLength);
 }
