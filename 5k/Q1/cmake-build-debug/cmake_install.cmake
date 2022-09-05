9ebf10e55c2f9d1e1a28ae523346f8c74e103a1c
FreeDoM <jstrfaheem065@gmail.com>
2022-09-05 21:43:19 +0530

    Clion path fix,

--- 5k/Q1/cmake-build-debug/cmake_install.cmake
+++ 5k/Q1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/freedom/Desktop/GitHub/CStudy/Q1
+# Install script for directory: /home/freedom/Desktop/GitHub/CStudy/5k/Q1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/freedom/Desktop/GitHub/CStudy/Q1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/freedom/Desktop/GitHub/CStudy/5k/Q1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
